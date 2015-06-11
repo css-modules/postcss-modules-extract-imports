9dc91c928ebec2aad0aa71544c71cc2b8940f1c0
Glen Maddern <glenmaddern@gmail.com>
2015-06-12 07:27:17 +1000

    added a test case to cover dashes and other chars in imported classes

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
 import postcss from 'postcss';
 
 const declWhitelist = ['composes'],
-  declFilter = new RegExp(`^(${declWhitelist.join('|')})$`),
-  matchImports = /^([\w\s]+?)\s+from\s+(?:"([^"]+)"|'([^']+)')$/;
+  declFilter = new RegExp( `^(${declWhitelist.join( '|' )})$` ),
+  matchImports = /^(.+?)\s+from\s+(?:"([^"]+)"|'([^']+)')$/;
 
-const processor = postcss.plugin('modules-extract-imports', function(options) {
-  return (css) => {
-    let imports = {};
-    let importIndex = 0;
-    let createImportedName = options && options.createImportedName || ((importName/*, path*/) => `i__imported_${importName}_${importIndex++}`);
+const processor = postcss.plugin( 'modules-extract-imports', function ( options ) {
+  return ( css ) => {
+    let imports = {},
+      importIndex = 0,
+      createImportedName = options && options.createImportedName || (( importName/*, path*/ ) => `i__imported_${importName.replace( /\W/g, '_' )}_${importIndex++}`);
 
     // Find any declaration that supports imports
-    css.eachDecl(declFilter, (decl) => {
-      let matches = decl.value.match(matchImports);
-      if (matches) {
+    css.eachDecl( declFilter, ( decl ) => {
+      let matches = decl.value.match( matchImports );
+      if ( matches ) {
         let [/*match*/, symbols, doubleQuotePath, singleQuotePath] = matches;
         let path = doubleQuotePath || singleQuotePath;
         imports[path] = imports[path] || {};
-        let tmpSymbols = symbols.split(/\s+/)
-          .map(s => {
-            if(!imports[path][s]) {
-              imports[path][s] = createImportedName(s, path);
+        let tmpSymbols = symbols.split( /\s+/ )
+          .map( s => {
+            if ( !imports[path][s] ) {
+              imports[path][s] = createImportedName( s, path );
             }
             return imports[path][s];
-          });
-        decl.value = tmpSymbols.join(' ');
+          } );
+        decl.value = tmpSymbols.join( ' ' );
       }
-    });
+    } );
 
     // If we've found any imports, insert :import rules
-    Object.keys(imports).reverse().forEach(path => {
+    Object.keys( imports ).reverse().forEach( path => {
       let pathImports = imports[path];
-      css.prepend(postcss.rule({
+      css.prepend( postcss.rule( {
         selector: `:import("${path}")`,
         after: "\n",
-        nodes: Object.keys(pathImports).map(importedSymbol => postcss.decl({
+        nodes: Object.keys( pathImports ).map( importedSymbol => postcss.decl( {
           value: importedSymbol,
           prop: pathImports[importedSymbol],
           before: "\n  "
-        }))
-      }));
-    });
+        } ) )
+      } ) );
+    } );
   };
-});
+} );
 
 export default processor;
