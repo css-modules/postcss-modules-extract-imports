7f4028cdd764aaa7a1b458567fe1febe555cdcd7
Alexey Litvinov <iamhoneyboy@gmail.com>
2017-05-22 09:39:36 +0300

    feature: predictable imports order (#138)
    * predictable imports order
    
    * test cases
    
    * added visited graph to handle copies
    
    * fallback for nodejs 4
    
    * testcase for duplicates
    
    * graph description
    
    * version 1.2.0

--- src/index.js
+++ src/index.js
@@ -1,69 +1,169 @@
 import postcss from 'postcss';
+import topologicalSort from './topologicalSort';
 
-const declWhitelist = ['composes'],
-  declFilter = new RegExp( `^(${declWhitelist.join( '|' )})$` ),
-  matchImports = /^(.+?)\s+from\s+(?:"([^"]+)"|'([^']+)'|(global))$/,
-  icssImport = /^:import\((?:"([^"]+)"|'([^']+)')\)/;
+const declWhitelist = ['composes'];
+const declFilter = new RegExp( `^(${declWhitelist.join( '|' )})$` );
+const matchImports = /^(.+?)\s+from\s+(?:"([^"]+)"|'([^']+)'|(global))$/;
+const icssImport = /^:import\((?:"([^"]+)"|'([^']+)')\)/;
 
-const processor = postcss.plugin( 'modules-extract-imports', function ( options ) {
-  return ( css ) => {
-    let imports = {},
-      importIndex = 0,
-      createImportedName = options && options.createImportedName || (( importName/*, path*/ ) => `i__imported_${importName.replace( /\W/g, '_' )}_${importIndex++}`);
+const VISITED_MARKER = 1;
 
-    // Find any declaration that supports imports
-    css.walkDecls( declFilter, ( decl ) => {
-      let matches = decl.value.match( matchImports );
-      let tmpSymbols;
-      if ( matches ) {
-        let [/*match*/, symbols, doubleQuotePath, singleQuotePath, global] = matches;
-        if (global) {
-          // Composing globals simply means changing these classes to wrap them in global(name)
-          tmpSymbols = symbols.split(/\s+/).map(s => `global(${s})`)
-        } else {
-          let path = doubleQuotePath || singleQuotePath;
-          imports[path] = imports[path] || {};
-          tmpSymbols = symbols.split(/\s+/)
-            .map(s => {
-              if (!imports[path][s]) {
-                imports[path][s] = createImportedName(s, path);
-              }
-              return imports[path][s];
-            });
-        }
-        decl.value = tmpSymbols.join( ' ' );
-      }
-    } );
+function createParentName(rule, root) {
+  return `__${root.index(rule.parent)}_${rule.selector}`;
+}
 
-    // If we've found any imports, insert or append :import rules
-    let existingImports = {};
+function serializeImports(imports) {
+  return imports.map(importPath => '`' + importPath + '`').join(', ');
+}
+
+/**
+ * :import('G') {}
+ *
+ * Rule
+ *   composes: ... from 'A'
+ *   composes: ... from 'B'
+
+ * Rule
+ *   composes: ... from 'A'
+ *   composes: ... from 'A'
+ *   composes: ... from 'C'
+ *
+ * Results in:
+ *
+ * graph: {
+ *   G: [],
+ *   A: [],
+ *   B: ['A'],
+ *   C: ['A'],
+ * }
+ */
+function addImportToGraph(importId, parentId, graph, visited) {
+  const siblingsId = parentId + '_' + 'siblings';
+  const visitedId = parentId + '_' + importId;
+
+  if (visited[visitedId] !== VISITED_MARKER) {
+    if (!Array.isArray(visited[siblingsId])) visited[siblingsId] = [];
+
+    const siblings = visited[siblingsId];
+
+    if (Array.isArray(graph[importId]))
+      graph[importId] = graph[importId].concat(siblings);
+    else
+      graph[importId] = siblings.slice();
+
+    visited[visitedId] = VISITED_MARKER;
+    siblings.push(importId);
+  }
+}
+
+const processor = postcss.plugin('modules-extract-imports', function (options = {}) {
+  const failOnWrongOrder = options.failOnWrongOrder;
+
+  return css => {
+    const graph = {};
+    const visited = {};
+
+    const existingImports = {};
+    const importDecls = {};
+    const imports = {};
+
+    let importIndex = 0;
+
+    const createImportedName = typeof options.createImportedName !== 'function'
+      ? (importName/*, path*/) => `i__imported_${importName.replace(/\W/g, '_')}_${importIndex++}`
+      : options.createImportedName;
+
+    // Check the existing imports order and save refs
     css.walkRules(rule => {
-      let matches = icssImport.exec(rule.selector);
+      const matches = icssImport.exec(rule.selector);
+
       if (matches) {
-        let [/*match*/, doubleQuotePath, singleQuotePath] = matches;
-        existingImports[doubleQuotePath || singleQuotePath] = rule;
+        const [/*match*/, doubleQuotePath, singleQuotePath] = matches;
+        const importPath = doubleQuotePath || singleQuotePath;
+
+        addImportToGraph(importPath, 'root', graph, visited);
+
+        existingImports[importPath] = rule;
       }
     });
 
-    Object.keys( imports ).reverse().forEach( path => {
+    // Find any declaration that supports imports
+    css.walkDecls(declFilter, decl => {
+      let matches = decl.value.match(matchImports);
+      let tmpSymbols;
 
-      let rule = existingImports[path];
-      if (!rule) {
-        rule = postcss.rule( {
-          selector: `:import("${path}")`,
-          raws: { after: "\n" }
-        } );
-        css.prepend( rule );
+      if (matches) {
+        let [/*match*/, symbols, doubleQuotePath, singleQuotePath, global] = matches;
+
+        if (global) {
+          // Composing globals simply means changing these classes to wrap them in global(name)
+          tmpSymbols = symbols.split(/\s+/).map(s => `global(${s})`);
+        } else {
+          const importPath = doubleQuotePath || singleQuotePath;
+          const parentRule = createParentName(decl.parent, css);
+
+          addImportToGraph(importPath, parentRule, graph, visited);
+
+          importDecls[importPath] = decl;
+          imports[importPath] = imports[importPath] || {};
+
+          tmpSymbols = symbols.split(/\s+/).map(s => {
+            if (!imports[importPath][s]) {
+              imports[importPath][s] = createImportedName(s, importPath);
+            }
+
+            return imports[importPath][s];
+          });
+        }
+
+        decl.value = tmpSymbols.join(' ');
       }
-      Object.keys( imports[path] ).forEach( importedSymbol => {
-        rule.append(postcss.decl( {
+    });
+
+    const importsOrder = topologicalSort(graph, failOnWrongOrder);
+
+    if (importsOrder instanceof Error) {
+      const importPath = importsOrder.nodes.find(importPath => importDecls.hasOwnProperty(importPath));
+      const decl = importDecls[importPath];
+
+      const errMsg = 'Failed to resolve order of composed modules ' + serializeImports(importsOrder.nodes) + '.';
+
+      throw decl.error(errMsg, {
+        plugin: 'modules-extract-imports',
+        word: 'composes',
+      });
+    }
+
+    let lastImportRule;
+    importsOrder.forEach(path => {
+      const importedSymbols = imports[path];
+      let rule = existingImports[path];
+
+      if (!rule && importedSymbols) {
+        rule = postcss.rule({
+          selector: `:import("${path}")`,
+          raws: {after: '\n'},
+        });
+
+        if (lastImportRule)
+          css.insertAfter(lastImportRule, rule);
+        else
+          css.prepend(rule);
+      }
+
+      lastImportRule = rule;
+
+      if (!importedSymbols) return;
+
+      Object.keys(importedSymbols).forEach(importedSymbol => {
+        rule.append(postcss.decl({
           value: importedSymbol,
-          prop: imports[path][importedSymbol],
-          raws: { before: "\n  " }
-        } ) );
-      } );
-    } );
+          prop: importedSymbols[importedSymbol],
+          raws: {before: '\n  '},
+        }));
+      });
+    });
   };
-} );
+});
 
 export default processor;
---
+++ test/check-import-order.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const assert = require('assert');
+const postcss = require('postcss');
+const processor = require('../');
+
+describe('check-import-order', () => {
+  let pipeline;
+
+  beforeEach(() => {
+    pipeline = postcss([
+      processor({failOnWrongOrder: true}),
+    ]);
+  });
+
+  it('should throw an exception', () => {
+    const input = `
+      .aa {
+        composes: b from './b.css';
+        composes: c from './c.css';
+      }
+
+      .bb {
+        composes: c from './c.css';
+        composes: b from './b.css';
+      }
+    `;
+
+    assert.throws(() => pipeline.process(input).css,
+      /Failed to resolve order of composed modules/);
+  });
+});
